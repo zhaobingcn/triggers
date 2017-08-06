a9720975c0c0771994d871ace06e36872f212b1f
zhaobingcn <zhaobingcn@hotmail.com>
2017-08-06 19:23:40 +0800

    修改完毕

--- src/main/java/com/neo4j/autoindex/AddIndexRunnable.java
+++ src/main/java/com/neo4j/autoindex/AddIndexRunnable.java
@@ -2,13 +2,23 @@ package com.neo4j.autoindex;
 
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.event.TransactionData;
+import org.neo4j.graphdb.index.Index;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.neo4j.graphdb.index.IndexManager;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.neo4j.helpers.collection.MapUtil.stringMap;
 
 public class AddIndexRunnable implements Runnable {
 
     private static TransactionData td;
     private static GraphDatabaseService db;
+    private static final Map<String,String> STANDARD_ANALYZER =
+            stringMap( IndexManager.PROVIDER, "lucene", "type", "fulltext", "analyzer", "org.apache.lucene.analysis.standard.StandardAnalyzer");
+
 
     public AddIndexRunnable(TransactionData transactionData, GraphDatabaseService graphDatabaseService) {
         td = transactionData;
@@ -24,23 +34,40 @@ public class AddIndexRunnable implements Runnable {
 //                    suspects.add(node);
 //                GmailSender.sendEmail("maxdemarzi@gmail.com", "A new Suspect has been created in the System!", "boo-yeah");
 
-                for(IndexInfo indexInfo: indexInfoList){
-                    for(Label label: node.getLabels()){
-                        if(indexInfo.containsIndexName(label.toString())){
+                IndexInfo infos = toBeIndexInfo(indexInfoList, node.getLabels());
 
-                        }
+                Set<Map.Entry<String, Object>> properties =
+                        node.getProperties(infos.getPropKeys()).entrySet();
+
+                for (String indexName : infos.getIndexNames()) {
+                    Index<Node> index = db.index().forNodes(indexName, STANDARD_ANALYZER);
+
+                    index.remove(node);
+
+                    // And then index all the properties
+                    for (Map.Entry<String, Object> property : properties) {
+                        index.add(node, property.getKey(), property.getValue());
                     }
                 }
 
-                tx.acquireWriteLock(node);
-
-                node.setProperty("age", "30");
+//                tx.acquireWriteLock(node);
+//
+//                node.setProperty("age", "30");
                 System.out.println("has add property");
-//                System.out.println("A new Suspect has been created!");
-//                }
-            }
-            tx.success();
+                tx.success();
 
+            }
         }
+    }
+
+    public IndexInfo toBeIndexInfo(List<IndexInfo> indexInfos, Iterable<Label> labels){
+        for(IndexInfo indexInfo: indexInfos){
+            for(Label label: labels){
+                if(indexInfo.containsIndexName(label.toString())){
+                    return indexInfo;
+                }
+            }
+        }
+        return null;
     }
 }
